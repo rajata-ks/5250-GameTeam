6d8837b4b8fecfce982d99dd342305547c028227
Nichlos Ho <hon1@seattleu.edu>
2022-01-30 14:07:01 -0800

    Copy code from MonsterCreatePage to MonsterUpdatePages

--- Game/Game/Views/Monsters/MonsterUpdatePage.xaml.cs
+++ Game/Game/Views/Monsters/MonsterUpdatePage.xaml.cs
@@ -22,6 +22,18 @@ namespace Game.Views
         // Hold the current location selected
         public ItemLocationEnum PopupLocationEnum = ItemLocationEnum.Unknown;
 
+        //Silder increment size
+        private readonly int SliderStepSize = 1;
+
+        //Bools used to validate name.
+        private bool nameValid;
+
+        //Bool used to validate description.
+        private bool descriptionValid;
+
+        //Bool used to validate image.
+        private bool imageValid;
+
         // Empty Constructor for UTs
         public MonsterUpdatePage(bool UnitTest) { }
 
@@ -35,6 +47,11 @@ namespace Game.Views
             BindingContext = this.ViewModel = data;
 
             this.ViewModel.Title = "Update " + data.Title;
+
+            //Defaults bools
+            nameValid = true;
+            descriptionValid = true;
+            imageValid = true;
 
             _ = UpdatePageBindingContext();
         }
@@ -64,6 +81,22 @@ namespace Game.Views
         /// <param name="e"></param>
         public async void Save_Clicked(object sender, EventArgs e)
         {
+            //Check to make sure all boxes are filled.
+            if (nameValid == false)
+            {
+                return;
+            }
+
+            if (descriptionValid == false)
+            {
+                return;
+            }
+
+            if (imageValid == false)
+            {
+                return;
+            }
+
             // If the image in the data box is empty, use the default one..
             if (string.IsNullOrEmpty(ViewModel.Data.ImageURI))
             {
@@ -83,6 +116,157 @@ namespace Game.Views
         public async void Cancel_Clicked(object sender, EventArgs e)
         {
             _ = await Navigation.PopModalAsync();
+        }
+
+        /// <summary>
+        /// Catch the change to the Stepper for Health
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void Health_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            double StepValue = SliderStepSize;
+
+            //rounding the value based on increments
+            var newStep = roundSilder(e.NewValue, healthSilder);
+
+            HealthValue.Text = String.Format("{0}", newStep);
+        }
+
+        /// <summary>
+        /// Catch the change to the Stepper for Attack
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void Attack_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            double StepValue = SliderStepSize;
+
+            //rounding the value based on increments
+            var newStep = roundSilder(e.NewValue, attackSilder);
+
+            AttackValue.Text = String.Format("{0}", newStep);
+        }
+
+        /// <summary>
+        /// Catch the change to the Stepper for Defense
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void Defense_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            //rounding the value based on increments
+            var newStep = roundSilder(e.NewValue, defenseSilder);
+
+            DefenseValue.Text = String.Format("{0}", newStep);
+        }
+
+        /// <summary>
+        /// Catch the change to the Stepper for Speed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void Speed_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            //rounding the value based on increments
+            var newStep = roundSilder(e.NewValue, speedSilder);
+
+            SpeedValue.Text = String.Format("{0}", newStep);
+        }
+
+
+        /// <summary>
+        /// Validate the entry for name.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void Name_TextChanged(object sender, ValueChangedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(NameEntry.Text))
+            {
+                NameLabel.TextColor = Color.Red;
+                NameLabel.Text = "Name*";
+                nameValid = false;
+
+                return;
+            }
+
+            if (String.IsNullOrWhiteSpace(NameEntry.Text))
+            {
+                NameLabel.TextColor = Color.Red;
+                NameLabel.Text = "Name*";
+                nameValid = false;
+
+                return;
+            }
+
+            NameLabel.TextColor = Color.Black;
+            NameLabel.Text = "Name*";
+            nameValid = true;
+        }
+
+        /// <summary>
+        /// Validate the entry for description.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void Description_TextChanged(object sender, ValueChangedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(DescriptionEntry.Text))
+            {
+                DescriptionLabel.TextColor = Color.Red;
+                DescriptionLabel.Text = "Description";
+                descriptionValid = false;
+
+                return;
+            }
+
+            if (String.IsNullOrWhiteSpace(DescriptionEntry.Text))
+            {
+                DescriptionLabel.TextColor = Color.Red;
+                DescriptionLabel.Text = "Description*";
+                descriptionValid = false;
+
+                return;
+            }
+
+            DescriptionLabel.TextColor = Color.Black;
+            DescriptionLabel.Text = "Description";
+            descriptionValid = true;
+        }
+
+        /// <summary>
+        /// Validate the entry for image.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void Image_TextChanged(object sender, ValueChangedEventArgs e)
+        {
+
+            if (String.IsNullOrEmpty(ImageEntry.Text))
+            {
+                ImageLabel.TextColor = Color.Red;
+                imageValid = false;
+                return;
+            }
+
+            if (String.IsNullOrWhiteSpace(ImageEntry.Text))
+            {
+                ImageLabel.TextColor = Color.Red;
+                imageValid = false;
+                return;
+            }
+
+            if (!ImageEntry.Text.EndsWith(".png"))
+            {
+                ImageLabel.TextColor = Color.Red;
+                imageValid = false;
+                return;
+            }
+
+            ImageLabel.TextColor = Color.Black;
+            imageValid = true;
+
         }
 
         ///// <summary>
@@ -110,5 +294,17 @@ namespace Game.Views
 
         //    return true;
         //}
+
+        private double roundSilder(double val, Slider slide)
+        {
+            if (slide == null)
+            {
+                return 0;
+            }
+            //rounding the value based on increments
+            var newStep = Math.Round(val / SliderStepSize);
+
+            return slide.Value = newStep * SliderStepSize;
+        }
     }
 }
