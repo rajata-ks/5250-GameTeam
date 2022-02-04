0795886537c6899664db816f9fc81397061e1a37
rajata-ks <78512151+rajata-ks@users.noreply.github.com>
2022-02-03 20:35:23 -0800

    Added GetItemToDisplay in MonsterReadPage.xaml.cs

--- Game/Game/Views/Monsters/MonsterReadPage.xaml.cs
+++ Game/Game/Views/Monsters/MonsterReadPage.xaml.cs
@@ -52,8 +52,65 @@ namespace Game.Views
 
         }
 
+        /// <summary>
+        /// Look up the Item to Display
+        /// </summary>
+        /// <param name="location"></param>
+        /// <returns></returns>
+        public StackLayout GetItemToDisplay(ItemLocationEnum location)
+        {
+            // Defualt Image is the Plus
+            var ImageSource = "icon_cancel.png";
+            var ClickableButton = true;
 
+            var data = ViewModel.Data.GetItemByLocation(location);
+            if (data == null)
+            {
+                // Show the Default Icon for the Location
+                data = new ItemModel { Location = location, ImageURI = ImageSource };
 
+                // Turn off click action
+                ClickableButton = false;
+            }
+
+            // Hookup the Image Button to show the Item picture
+            var ItemButton = new ImageButton
+            {
+                Style = (Style)Application.Current.Resources["ImageMediumStyle"],
+                Source = data.ImageURI
+            };
+
+            if (ClickableButton)
+            {
+                // Add a event to the user can click the item and see more
+                ItemButton.Clicked += (sender, args) => ShowPopup(data);
+            }
+
+            // Add the Display Text for the item
+            var ItemLabel = new Label
+            {
+                Text = location.ToMessage(),
+                Style = (Style)Application.Current.Resources["ValueStyleMicro"],
+                HorizontalOptions = LayoutOptions.Center,
+                HorizontalTextAlignment = TextAlignment.Center
+            };
+
+            // Put the Image Button and Text inside a layout
+            var ItemStack = new StackLayout
+            {
+                Margin = 3,
+                Style = (Style)Application.Current.Resources["ItemImageLabelBox"],
+                HorizontalOptions = LayoutOptions.Center,
+                Children = {
+                    ItemButton,
+                    ItemLabel
+                },
+            };
+
+            return ItemStack;
+        }
+
+       
 
         /// <summary>
         /// Save calls to Update
