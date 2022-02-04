e0f0031d9970780f734d03ba368af41f4b213726
rajata-ks <78512151+rajata-ks@users.noreply.github.com>
2022-02-03 20:37:43 -0800

    Added ShowPopup() in MonsterReadPage.xaml.cs

--- Game/Game/Views/Monsters/MonsterReadPage.xaml.cs
+++ Game/Game/Views/Monsters/MonsterReadPage.xaml.cs
@@ -110,7 +110,32 @@ namespace Game.Views
             return ItemStack;
         }
 
-       
+        /// <summary>
+        /// Show the Popup for the Item
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public bool ShowPopup(ItemModel data)
+        {
+            PopupLoadingView.IsVisible = true;
+            PopupItemImage.Source = data.ImageURI;
+
+            PopupItemName.Text = data.Name;
+            PopupItemDescription.Text = data.Description;
+            PopupItemLocation.Text = data.Location.ToMessage();
+            PopupItemAttribute.Text = data.Attribute.ToMessage();
+            PopupItemValue.Text = " + " + data.Value.ToString();
+
+            //// Close the popup after 3 seconds
+            //Device.StartTimer(TimeSpan.FromSeconds(3), () => { 
+            //        PopupLoadingView.IsVisible = false;
+            //        return true; }); 
+
+            return true;
+        }
+
+        
+
 
         /// <summary>
         /// Save calls to Update
