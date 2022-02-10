bf591257f664ec88e4e6ded4bad282653ad40e56
rajata-ks <78512151+rajata-ks@users.noreply.github.com>
2022-02-10 12:01:29 -0800

    Chnaged text colors in MonsterUpdatePage.xaml

--- Game/Game/Views/Monsters/MonsterUpdatePage.xaml.cs
+++ Game/Game/Views/Monsters/MonsterUpdatePage.xaml.cs
@@ -315,7 +315,7 @@ namespace Game.Views
                 return;
             }
 
-            NameLabel.TextColor = Color.Black;
+            NameLabel.TextColor = Color.White;
             NameLabel.Text = "Name*";
             nameValid = true;
         }
@@ -345,7 +345,7 @@ namespace Game.Views
                 return;
             }
 
-            DescriptionLabel.TextColor = Color.Black;
+            DescriptionLabel.TextColor = Color.White;
             DescriptionLabel.Text = "Description";
             descriptionValid = true;
         }
@@ -379,7 +379,7 @@ namespace Game.Views
                 return;
             }
 
-            ImageLabel.TextColor = Color.Black;
+            ImageLabel.TextColor = Color.White;
             imageValid = true;
 
         }
