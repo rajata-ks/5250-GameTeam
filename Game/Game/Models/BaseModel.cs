e79e389f7a4f99f4cdb60ea239a0fc4fff6bdf75
Nichlos Ho <hon1@seattleu.edu>
2022-02-15 11:30:56 -0800

    removed variable on basemodel.cs

--- Game/Game/Models/BaseModel.cs
+++ Game/Game/Models/BaseModel.cs
@@ -6,9 +6,6 @@ namespace Game.Models
     /// </summary>
     public class BaseModel<T> : DefaultModel
     {
-        // Location to the image for the item.  Will come from the server as a fully qualified URI example:  https://developer.android.com/images/robot-tiny.png
-        public string ImageURI { get; set; } = Services.ItemService.DefaultImageURI;
-
         /// <summary>
         /// Update Method is virutal and changed for each class
         /// </summary>
