29cccb2a3090fa4602cc5bc53a02a13b77bbf6d9
Nichlos Ho <hon1@seattleu.edu>
2022-02-15 16:10:06 -0800

    remove image valid on characterupdatepage.xaml.cs

--- Game/Game/Views/Characters/CharacterUpdatePage.xaml.cs
+++ Game/Game/Views/Characters/CharacterUpdatePage.xaml.cs
@@ -138,11 +138,6 @@ namespace Game.Views
                 return;
             }
 
-            if (imageValid == false)
-            {
-                return;
-            }
-
             // If the image in the data box is empty, use the default one..
             if (string.IsNullOrEmpty(ViewModel.Data.ImageURI))
             {
@@ -539,7 +534,7 @@ namespace Game.Views
         /// <param name="val"></param>
         /// <param name="slide"> silder object</param>
         /// <returns></returns>
-        private double RoundSilderValueToWhole(double val, Slider slide)
+        public double RoundSilderValueToWhole(double val, Slider slide)
         {
             if (slide == null)
             {
