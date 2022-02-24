09eeed2de4d35c8237b8ac2a92b641bc0236b542
nakamurabrad@seattleu.edu <70090205+BradMNakamura@users.noreply.github.com>
2022-02-24 15:56:32 -0800

    Added Item Frame to PickItemsPage.xaml.cs

--- Game/Game/Views/Battle/PickItemsPage.xaml.cs
+++ Game/Game/Views/Battle/PickItemsPage.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Game.Models;
+using Game.ViewModels;
+using System;
+using System.Linq;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+
 
 namespace Game.Views
 {
@@ -16,7 +20,115 @@ namespace Game.Views
         public PickItemsPage()
         {
             InitializeComponent();
+
+            //DrawCharacterList();
+            DrawDroppedItems();
         }
+
+      
+
+
+        /// <summary>
+        /// Add the Dropped Items to the Display
+        /// </summary>
+        public void DrawDroppedItems()
+        {
+            // Clear and Populate the Dropped Items
+            var FlexList = ItemListFoundFrame.Children.ToList();
+            foreach (var data in FlexList)
+            {
+                _ = ItemListFoundFrame.Children.Remove(data);
+            }
+
+            foreach (var data in BattleEngineViewModel.Instance.Engine.EngineSettings.BattleScore.ItemModelDropList.Distinct())
+            {
+                ItemListFoundFrame.Children.Add(GetItemToDisplay(data));
+            }
+        }
+        /// <summary>
+        /// Look up the Item to Display
+        /// </summary>
+        /// <param name="location"></param>
+        /// <returns></returns>
+        public StackLayout GetItemToDisplay(ItemModel item)
+        {
+            if (item == null)
+            {
+                return new StackLayout();
+            }
+
+            if (string.IsNullOrEmpty(item.Id))
+            {
+                return new StackLayout();
+            }
+
+            // Defualt Image is the Plus
+            var ClickableButton = true;
+
+            var data = ItemIndexViewModel.Instance.GetItem(item.Id);
+            if (data == null)
+            {
+                // Show the Default Icon for the Location
+                data = new ItemModel { Name = "Unknown", ImageURI = "icon_cancel.png" };
+
+                // Turn off click action
+                ClickableButton = false;
+            }
+
+            // Hookup the Image Button to show the Item picture
+            var ItemButton = new ImageButton
+            {
+                Style = (Style)Application.Current.Resources["ImageMediumStyle"],
+                Source = data.ImageURI
+            };
+
+            if (ClickableButton)
+            {
+                // Add a event to the user can click the item and see more
+                ItemButton.Clicked += (sender, args) => ShowPopup(data);
+            }
+
+            // Put the Image Button and Text inside a layout
+            var ItemStack = new StackLayout
+            {
+                Padding = 3,
+                Style = (Style)Application.Current.Resources["ItemImageBox"],
+                HorizontalOptions = LayoutOptions.Center,
+                Children = {
+                    ItemButton,
+                },
+            };
+
+            return ItemStack;
+        }
+        /// <summary>
+        /// Show the Popup for the Item
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public bool ShowPopup(ItemModel data)
+        {
+            PopupLoadingView.IsVisible = true;
+            PopupItemImage.Source = data.ImageURI;
+
+            PopupItemName.Text = data.Name;
+            PopupItemDescription.Text = data.Description;
+            PopupItemLocation.Text = data.Location.ToMessage();
+            PopupItemAttribute.Text = data.Attribute.ToMessage();
+            PopupItemValue.Text = " + " + data.Value.ToString();
+            return true;
+        }
+
+        /// <summary>
+        /// Close the popup
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void ClosePopup_Clicked(object sender, EventArgs e)
+        {
+            PopupLoadingView.IsVisible = false;
+        }
+
         /// <summary>
         /// Quit the Battle
         /// 
@@ -28,5 +140,7 @@ namespace Game.Views
         {
             _ = await Navigation.PopModalAsync();
         }
+
+
     }
 }
