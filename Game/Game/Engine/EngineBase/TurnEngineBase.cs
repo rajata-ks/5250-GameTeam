bc758da4f548752067f8031afab9af1da80ac123
Brad Nakamura <bradmnakamura@gmail.com>
2022-03-01 17:06:29 -0800

    Added logic to make Doug always miss in TurnEngineBase.cs

--- Game/Game/Engine/EngineBase/TurnEngineBase.cs
+++ Game/Game/Engine/EngineBase/TurnEngineBase.cs
@@ -381,6 +381,12 @@ namespace Game.Engine.EngineBase
             // See if the Battle Settings Overrides the Roll
             EngineSettings.BattleMessagesModel.HitStatus = BattleSettingsOverride(Attacker);
 
+            if(Attacker.Name == "Doug")
+            {
+                EngineSettings.BattleMessagesModel.HitStatus = HitStatusEnum.Miss;
+
+            }
+
             switch (EngineSettings.BattleMessagesModel.HitStatus)
             {
                 case HitStatusEnum.Miss:
