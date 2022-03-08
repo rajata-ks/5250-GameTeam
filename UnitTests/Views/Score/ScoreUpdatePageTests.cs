2875a1fd709848541def75ec91b5dd552c4a534d
rajata-ks <78512151+rajata-ks@users.noreply.github.com>
2022-03-08 12:34:59 -0800

    Added UT's for Delte_Button in ScoreUpdatePageTests.cs

--- UnitTests/Views/Score/ScoreUpdatePageTests.cs
+++ UnitTests/Views/Score/ScoreUpdatePageTests.cs
@@ -64,6 +64,20 @@ namespace UnitTests.Views
             // Assert
             Assert.IsTrue(true); // Got to here, so it happened...
         }
+        
+        [Test]
+        public void ScoreUpdatePage_Delete_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.Delete_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
 
         [Test]
         public void ScoreUpdatePage_Save_Clicked_Default_Should_Pass()
