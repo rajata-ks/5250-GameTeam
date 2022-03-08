7b040783b7071043122595001ec68232dc77aebc
nakamurabrad@seattleu.edu <70090205+BradMNakamura@users.noreply.github.com>
2022-03-08 14:12:27 -0800

    Added logic for character items in PickItemPage.xaml.cs

--- Game/Game/Views/Battle/PickItemsPage.xaml.cs
+++ Game/Game/Views/Battle/PickItemsPage.xaml.cs
@@ -16,6 +16,14 @@ namespace Game.Views
     {
         //Parallel list used to get characters from list view.
         public List<PlayerInfoModel> characterList;
+
+        // Hold the current location selected
+        public ItemLocationEnum PopupLocationEnum = ItemLocationEnum.Unknown;
+        // The Character to create
+        public GenericViewModel<PlayerInfoModel> ViewModel { get; set; }
+
+        //Keep track of current character index
+        public int currentCharacterIndex;
         /// <summary>
         /// Constructor
         /// </summary>
@@ -24,9 +32,10 @@ namespace Game.Views
             InitializeComponent();
 
             characterList = new List<PlayerInfoModel>();
-            
+            ViewModel = new GenericViewModel<PlayerInfoModel>();
+            currentCharacterIndex = 0;
             DrawCharacterList();
-            
+            AddItemsToDisplay();
             DrawDroppedItems();
         }
 
@@ -52,6 +61,7 @@ namespace Game.Views
         {
             Test.Text = e.SelectedItemIndex.ToString();
             PlayerInfoModel data = characterList[e.SelectedItemIndex];
+            currentCharacterIndex = e.SelectedItemIndex;
             CharacterImage.Source = data.ImageURI;
         }
 
@@ -68,6 +78,158 @@ namespace Game.Views
         }
 
         #region CharaterItems
+        /// <summary>
+        /// The row selected from the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0019:Use pattern matching", Justification = "<Pending>")]
+        public void OnPopupItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            ItemModel data = args.SelectedItem as ItemModel;
+            if (data == null)
+            {
+                return;
+            }
+
+            _ = ViewModel.Data.AddItem(PopupLocationEnum, data.Id);
+
+            AddItemsToDisplay();
+
+            ClosePopup();
+        }
+
+        /// <summary>
+        /// Show the Popup for Selecting Items
+        /// </summary>
+        /// <param name="location"></param>
+        /// <returns></returns>
+        public bool ShowPopup(ItemLocationEnum location)
+        {
+            PopupItemSelector.IsVisible = true;
+
+            PopupLocationLabel.Text = "Items for :";
+            PopupLocationValue.Text = location.ToMessage();
+
+            // Make a fake item for None
+            var NoneItem = new ItemModel
+            {
+                Id = null, // will use null to clear the item
+                Guid = "None", // how to find this item amoung all of them
+                ImageURI = "icon_cancel.png",
+                Name = "None",
+                Description = "None"
+            };
+
+            List<ItemModel> itemList = new List<ItemModel>
+            {
+                NoneItem
+            };
+
+            // Add the rest of the items to the list
+            itemList.AddRange(ItemIndexViewModel.Instance.GetLocationItems(location));
+
+            // Populate the list with the items
+            PopupLocationItemListView.ItemsSource = itemList;
+
+            // Remember the location for this popup
+            PopupLocationEnum = location;
+
+            return true;
+        }
+
+        /// <summary>
+        /// When the user clicks the close in the Popup
+        /// hide the view
+        /// show the scroll view
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void ClosePopup_Clicked(object sender, EventArgs e)
+        {
+            ClosePopup();
+        }
+
+        /// <summary>
+        /// Close the popup
+        /// </summary>
+        public void ClosePopup()
+        {
+            PopupItemSelector.IsVisible = false;
+        }
+
+        /// <summary>
+        /// Show the Items the Character has
+        /// </summary>
+        public void AddItemsToDisplay()
+        {
+            var FlexList = ItemBox.Children.ToList();
+            foreach (var data in FlexList)
+            {
+                _ = ItemBox.Children.Remove(data);
+            }
+
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.Head));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.Necklass));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.PrimaryHand));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.OffHand));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.RightFinger));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.LeftFinger));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.Feet));
+        }
+
+        /// <summary>
+        /// Look up the Item to Display
+        /// </summary>
+        /// <param name="location"></param>
+        /// <returns></returns>
+        public StackLayout GetItemToDisplay(ItemLocationEnum location)
+        {
+            // Get the Item, if it exist show the info
+            // If it does not exist, show a Plus Icon for the location
+
+            // Defualt Image is the Plus
+            var ImageSource = "icon_add.png";
+            ViewModel.Data = characterList[currentCharacterIndex];
+            var data = ViewModel.Data.GetItemByLocation(location);
+            if (data == null)
+            {
+                data = new ItemModel { Location = location, ImageURI = ImageSource };
+            }
+
+            // Hookup the Image Button to show the Item picture
+            var ItemButton = new ImageButton
+            {
+                Style = (Style)Application.Current.Resources["ImageMediumStyle"],
+                Source = data.ImageURI
+            };
+
+            // Add a event to the user can click the item and see more
+            ItemButton.Clicked += (sender, args) => ShowPopup(location);
+
+            // Add the Display Text for the item
+            var ItemLabel = new Label
+            {
+                Text = location.ToMessage(),
+                Style = (Style)Application.Current.Resources["ValueStyleMicro"],
+                HorizontalOptions = LayoutOptions.Center,
+                HorizontalTextAlignment = TextAlignment.Center
+            };
+
+            // Put the Image Button and Text inside a layout
+            var ItemStack = new StackLayout
+            {
+                Padding = 3,
+                Style = (Style)Application.Current.Resources["ItemImageLabelBox"],
+                HorizontalOptions = LayoutOptions.Center,
+                Children = {
+                    ItemButton,
+                    ItemLabel
+                },
+            };
+
+            return ItemStack;
+        }
 
         #endregion CharacterItems
 
@@ -169,7 +331,7 @@ namespace Game.Views
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        public void ClosePopup_Clicked(object sender, EventArgs e)
+        public void ClosePoolPopup_Clicked(object sender, EventArgs e)
         {
             PopupPoolItem.IsVisible = false;
         }
